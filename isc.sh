4ec843ac3e23ad70033b01b09bb523ccea6bd830
Will Berman <WLBberman@gmail.com>
2023-12-14 02:31:44 +0000

    remove ported

--- isc.sh
+++
@@ -1,5 +0,0 @@
-fidelity --gpu 0 --isc --input1 ~/out/muse_512/1.5 --input2 ~/val2017/  --samples-resize-and-crop 512 &> isc-1.5.txt & 
-fidelity --gpu 0 --isc --input1 ~/out/muse_512/2.0 --input2 ~/val2017/  --samples-resize-and-crop 512 &> isc-2.0.txt &
-fidelity --gpu 0 --isc --input1 ~/out/muse_512/3.0 --input2 ~/val2017/  --samples-resize-and-crop 512 &> isc-3.0.txt &
-fidelity --gpu 0 --isc --input1 ~/out/muse_512/4.0 --input2 ~/val2017/  --samples-resize-and-crop 512 &> isc-4.0.txt &
-fidelity --gpu 0 --isc --input1 ~/out/muse_512/5.0 --input2 ~/val2017/  --samples-resize-and-crop 512 &> isc-5.0.txt &
