c78b061a1ed75c7cbc2a88f659a9e1b1ad93b58c
Will Berman <WLBberman@gmail.com>
2023-12-19 03:58:27 +0000

    save to root

--- artifacts.py
+++ artifacts.py
@@ -18,7 +18,7 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 cli_config = OmegaConf.merge(dict(config_file=os.path.join(cur_dir, "img_stats.yaml")), OmegaConf.from_cli())
 config = OmegaConf.merge(
-    dict(only=None, skip={}, skip_guidance_scales=[]),
+    dict(only=None, skip={}, skip_guidance_scales=[], save_to_root=False),
     OmegaConf.load(cli_config.config_file), 
     cli_config,
 )
@@ -40,6 +40,9 @@ else:
 
 if isinstance(config.skip, str):
     config.skip = json.loads(config.skip)
+
+if config.save_to_root:
+    os.makedirs(os.path.join(cur_dir, 'charts'), exist_ok=True)
 
 def main():
     for stats_idx in range(len(config.stats)):
@@ -162,6 +165,7 @@ def make_clip_chart():
     logger.warning(f"making clip chart")
 
     plt.figure()
+    plt.grid(True)
     plt.title(f"CLIP Score")
     plt.ylabel("CLIP Score (10k)")
     plt.xlabel("cfg scale")
@@ -169,9 +173,9 @@ def make_clip_chart():
     for it in iter_helper():
         plt.plot(it["guidance_scales"], it["clip_scores"], marker="o", label=it["label"])
 
-
-    plt.legend()
     plt.savefig(os.path.join(cur_dir, config.save_to.path, config.run_prefix + '_clip.png'))
+    if config.save_to_root:
+        plt.savefig(os.path.join(cur_dir, 'charts', 'clip.png'))
     plt.close()
 
 def make_clip_table():
@@ -197,6 +201,7 @@ def make_fid_chart():
     logger.warning(f"making fid chart")
 
     plt.figure()
+    plt.grid(True)
     plt.title(f"FID")
     plt.ylabel("FID Score (10k)")
     plt.xlabel("cfg scale")
@@ -204,8 +209,9 @@ def make_fid_chart():
     for it in iter_helper():
         plt.plot(it["guidance_scales"], it["fid_scores"], marker="o", label=it["label"])
 
-    plt.legend()
     plt.savefig(os.path.join(cur_dir, config.save_to.path, config.run_prefix + '_fid.png'))
+    if config.save_to_root:
+        plt.savefig(os.path.join(cur_dir, 'charts', 'fid.png'))
     plt.close()
 
 def make_fid_table():
@@ -231,6 +237,7 @@ def make_isc_chart():
     logger.warning(f"making fid chart")
 
     plt.figure()
+    plt.grid(True)
     plt.title(f"Inception Score")
     plt.ylabel("Inception Score (10k)")
     plt.xlabel("cfg scale")
@@ -238,8 +245,9 @@ def make_isc_chart():
     for it in iter_helper():
         plt.plot(it["guidance_scales"], it["isc_scores"], marker="o", label=it["label"])
 
-    plt.legend()
     plt.savefig(os.path.join(cur_dir, config.save_to.path, config.run_prefix + '_isc.png'))
+    if config.save_to_root:
+        plt.savefig(os.path.join(cur_dir, 'charts', 'isc.png'))
     plt.close()
 
 def make_isc_table():
@@ -265,6 +273,7 @@ def make_fid_vs_clip_chart():
     logger.warning(f"making fid vs clip chart")
 
     plt.figure()
+    plt.grid(True)
     plt.title(f"FID vs CLIP")
     plt.ylabel("FID Score (10k)")
     plt.xlabel("CLIP Score (10k)")
@@ -274,6 +283,9 @@ def make_fid_vs_clip_chart():
 
     plt.legend()
     plt.savefig(os.path.join(cur_dir, config.save_to.path, config.run_prefix + '_fid_vs_clip.png'))
+    if config.save_to_root:
+        plt.savefig(os.path.join(cur_dir, 'charts', 'fid_vs_clip.png'))
+    plt.close()
 
 def combine_tables(clip_table, fid_table, isc_table):
     assert clip_table is not None and fid_table is not None and isc_table is not None
